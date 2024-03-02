98c00dd764b0270e047d3f45abf1f4310461d6f0
Ismail Assil <iassil@e3r9p15.1337.ma>
2024-03-02 17:52:14 +0100

    fixing 25% of export_function

--- builtin_ft/ft_export.c
+++ builtin_ft/ft_export.c
@@ -6,11 +6,18 @@
 /*   By: iassil <iassil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 17:47:07 by iassil            #+#    #+#             */
-/*   Updated: 2024/03/02 16:56:29 by iassil           ###   ########.fr       */
+/*   Updated: 2024/03/02 17:51:31 by iassil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+/*
+*	This Code is about 75%/100% it only need some adjustement like:
+*	export hello"hwol"=asdf		==>	hellohwol=asdf
+*	export hello				==>	hello=
+*/
+
 
 int	ft_fetch_arguments(char *arg, char **name, char **value)
 {
@@ -18,25 +25,22 @@ int	ft_fetch_arguments(char *arg, char **name, char **value)
 	int	i;
 
 	i = 0;
-	while (arg[i] != '\0')
-	{
-		index = 0;
-		while (arg[i] != '=' && arg[i] != '\0')
-			(1) && (index++, i++);
-		if (arg[i] == '=')
-			i++;
-		*name = ft_substr(arg, i - index, index);
-		if (!*name)
-			return (1);
-		if (arg[i] == '\'' || arg[i] == '\"')
-			i++;
-		index = 0;
-		while ((arg[i] != '\'' || arg[i] != '\"') && arg[i] != '\0')
-			(1) && (index++, i++);
-		*value = ft_substr(arg, i - index, index);
-		if (!*value)
-			return (1);
-	}
+	index = 0;
+	while (arg[i] != '=' && arg[i] != '\0')
+		(1) && (index++, i++);
+	if (arg[i] == '=')
+		(i++, index++);
+	*name = ft_substr(arg, i - index, index);
+	if (!*name)
+		return (1);
+	if (arg[i] == '\'' || arg[i] == '\"')
+		i++;
+	index = 0;
+	while (arg[i] != '\'' && arg[i] != '\"' && arg[i] != '\0')
+		(1) && (index++, i++);
+	*value = ft_substr(arg, i - index, index);
+	if (!*value)
+		return (1);
 	return (0);
 }
 
@@ -50,21 +54,23 @@ size_t	ft_strlenptr(char **envp)
 	return (i);
 }
 
-static void	ft_check_env(char ***envp, char **env, char *name, char *value)
+static void	ft_check_env(char **envp, char ***env, char **name, char **value)
 {
 	int	i;
+	int	len;
 
+	len = ft_strlenptr(*env);
 	i = 0;
-	(*envp) = (char **)malloc((ft_strlenptr(env) + 2) * sizeof(char *));
-	if (!(*envp))
+	envp = (char **)malloc((len + 2) * sizeof(char *));
+	if (!envp)
 		(write(2, "Error: Allocation failed\n", 25), exit(EXIT_FAILURE));
-	(*envp)[ft_strlenptr(env) + 2] = NULL;
-	while (env[i] != NULL)
-		(1) && ((*envp)[i] = ft_strdup(env[i]), i++);
-	(*envp)[i] = ft_strjoin(name, value);
-	if (!(*envp)[i])
+	envp[len + 2] = NULL;
+	while ((*env)[i] != NULL)
+		(1) && (envp[i] = ft_strdup((*env)[i]), i++);
+	envp[i] = ft_strjoin(*name, *value);
+	if (!envp[i])
 		(write(2, "Error: Allocation failed\n", 25), exit(EXIT_FAILURE));
-	env = (*envp);
+	*env = envp;
 }
 
 int	ft_export(char *argument, char ***env)
@@ -88,13 +94,13 @@ int	ft_export(char *argument, char ***env)
 		}
 		i++;
 	}
-	ft_check_env(&envp, *env, name, value);
+	ft_check_env(envp, env, &name, &value);
 	return (0);
 }
 
 int	main(int ac, char **av, char **env)
 {
-	if (ft_export("SAYMYNAME", &env) == 1)
+	if (ft_export("name\"value\"", &env) == 1)
 		printf("Error\n");
 	while (*env)
 		(printf("%s\n", *env), env++);
