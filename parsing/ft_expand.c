83629d0e4651d8c45cc658f7ba35dad36363e266
Ismail Assil <iassil@e3r9p13.1337.ma>
2024-03-08 16:35:37 +0100

    Handling the Norm

--- parsing/ft_expand.c
+++ parsing/ft_expand.c
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_expand.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aibn-che <aibn-che@student.42.fr>          +#+  +:+       +#+        */
+/*   By: iassil <iassil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 10:36:20 by iassil            #+#    #+#             */
-/*   Updated: 2024/03/08 16:11:10 by aibn-che         ###   ########.fr       */
+/*   Updated: 2024/03/08 16:33:54 by iassil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+/*
+*	Checks if the expanded variable exists in the env
+*/
 static char	*ft_arg_is_exist(t_env *env, char *var)
 {
 	t_env	*head;
@@ -47,65 +50,11 @@ static int	ft_surpass_chars(char *var)
 	return (i + 1);
 }
 
-char	*ftt_strjoin(char *str1, char *str2)
-{
-	int		len1;
-	int		len2;
-	char	*new_str;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	len1 = ft_strlen(str1);
-	len2 = ft_strlen(str2);
-	new_str = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (!new_str)
-		return (NULL);
-	while (str1 && str1[i])
-	{
-		new_str[i] = str1[i];
-		i++;
-	}
-	while (str2 && str2[j])
-	{
-		new_str[i] = str2[j];
-		(1) && (i++, j++);
-	}
-	new_str[i] = '\0';
-	return (new_str);
-}
-
-int	handle_inregulare_cases(t_expand *exp, int c, int *i)
-{
-	char	*s;
-
-	s = exp->new_str;
-	if (ft_isdigit(c))
-	{
-		if (c == '0')
-		{
-			exp->new_str = ftt_strjoin(s, "minishell");
-			free(s);
-		}
-		return ((*i += 2), 1);
-	}
-	else if (!ft_check_if_chars_digit(c))
-	{
-		if (c == '-')
-		{
-			exp->new_str = ftt_strjoin(s, "himBH");
-			*i += 1;
-			free(s);
-		}
-		else
-			ft_append_char(&exp->new_str, '$');
-		return ((*i += 1), 1);
-	}
-	return (0);
-}
-
-void	ft_update_quote(char *arg, int *i, t_expand *exp)
+/*
+*	Define the quote,
+*	that should not be included within the string
+*/
+static void	ft_update_quote(char *arg, int *i, t_expand *exp)
 {
 	if (exp->quote == arg[*i])
 	{
@@ -119,6 +68,9 @@ void	ft_update_quote(char *arg, int *i, t_expand *exp)
 	}
 }
 
+/*
+*	This function expands the variables
+*/
 static char	*ft_handle_expand(t_env *env, char *arg)
 {
 	int			i;
@@ -135,7 +87,7 @@ static char	*ft_handle_expand(t_env *env, char *arg)
 			ft_append_char(&exp.new_str, arg[i++]);
 		else if (arg[i] && arg[i] == '$' && exp.quote != '\'')
 		{
-			if (handle_inregulare_cases(&exp, arg[i + 1], &i))
+			if (ft_handle_inregulare_cases(&exp, arg[i + 1], &i))
 				continue ;
 			exp.expa = ft_arg_is_exist(env, arg + (i + 1));
 			exp.s = exp.new_str;
@@ -148,6 +100,9 @@ static char	*ft_handle_expand(t_env *env, char *arg)
 	return (exp.new_str);
 }
 
+/*
+*	This function expands the Variables from the env
+*/
 void	ft_expand_argument(t_env *env, t_token **linked_list)
 {
 	t_token	*head;
