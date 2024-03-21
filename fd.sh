a37264f69822043ad68d249fde5ae0da94741ba7
Ismail Assil <iassil@e3r11p8.1337.ma>
2024-03-21 16:07:04 +0000

    modifying the script fd.sh

--- fd.sh
+++ fd.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-# Find the PID of the minishell process
-pid=$(ps > fd && < fd grep ./minishell | awk '{print $1}' && rm -f fd)
-
-# Check if the PID was found
-if [ -n "$pid" ]; then
-    echo "PID of minishell process: $pid"
-    echo "Open files for minishell process:"
-    lsof -p "$pid"
-else
-    echo "minishell process not found."
-fi
+while true; do
+    echo -e "\e[1;33mOpened file descriptor for [./minishell]\e[0m"
+    lsof -c minishell | tail -n 4
+    echo
+    sleep 3
+done
