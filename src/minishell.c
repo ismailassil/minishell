1f9eef1c64bc06a4ebe8b023f00a13af3423695c
Ismail Assil <iassil@e3r11p10.1337.ma>
2024-03-24 22:24:06 +0000

    commenting ft_print

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: iassil <iassil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:20:06 by iassil            #+#    #+#             */
-/*   Updated: 2024/03/24 21:49:41 by iassil           ###   ########.fr       */
+/*   Updated: 2024/03/24 22:15:03 by iassil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,10 @@ void	ft_parse_input_from_shell(t_env *env, char *input)
 		return ;
 	}
 	(ft_remove_quotes(&head), ft_expand_argument(env, &head));
-	(ft_print_types(head), ft_print(head));
 	ft_execution(&head, env);
 }
 
+// (ft_print_types(head), ft_print(head));
 // void leaks()
 // {
 //     fclose(gfp);
