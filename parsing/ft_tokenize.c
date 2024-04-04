a4a59de72b18a44e41503c8cbf2a4861d254995c
Ismail Assil <iassil@e3r11p4.1337.ma>
2024-04-04 05:43:52 +0000

    fixing something in the parsing {tokenizing}

--- parsing/ft_tokenize.c
+++ parsing/ft_tokenize.c
@@ -6,7 +6,7 @@
 /*   By: iassil <iassil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 21:05:24 by iassil            #+#    #+#             */
-/*   Updated: 2024/03/17 17:57:50 by iassil           ###   ########.fr       */
+/*   Updated: 2024/04/04 05:24:52 by iassil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,11 +51,33 @@ void	ft_tokenize(t_token **str)
 	}
 }
 
+static void	ft_check_else(t_token *head, int *flag)
+{
+	if (ft_strncmp(head->token, ">>", ft_strlen(head->token)) == 0)
+	{
+		if (head->next != NULL)
+			head->next->type = FILENAME;
+		head->type = APPEND;
+	}
+	else if (ft_strncmp(head->token, "<<", ft_strlen(head->token)) == 0)
+	{
+		if (head->next != NULL)
+			head->next->type = DELIMITER;
+		head->type = HEREDOC;
+	}
+	else if (ft_strncmp(head->token, "|", ft_strlen(head->token)) == 0)
+		(1) && (head->type = PIPE, *flag = 0);
+	else
+		ft_check_rest_of_else(head, flag);
+}
+
 static void	ft_check_rest_of_else(t_token *head, int *flag)
 {
 	if (head->type == FILENAME || head->type == DELIMITER || head->type == ARG)
 	{
 		if (*flag == 1 && head->type == FILENAME && head->next != NULL)
+			head->next->type = ARG;
+		if (*flag == 1 && head->type == DELIMITER && head->next != NULL)
 			head->next->type = ARG;
 		if (head->type == ARG && head->next != NULL)
 			head->next->type = ARG;
@@ -69,24 +91,4 @@ static void	ft_check_rest_of_else(t_token *head, int *flag)
 		if (head->next != NULL)
 			head->next->type = ARG;
 	}
-}
-
-static void	ft_check_else(t_token *head, int *flag)
-{
-	if (ft_strncmp(head->token, ">>", ft_strlen(head->token)) == 0)
-	{
-		if (head->next != NULL)
-			head->next->type = FILENAME;
-		head->type = APPEND;
-	}
-	else if (ft_strncmp(head->token, "<<", ft_strlen(head->token)) == 0)
-	{
-		if (head->next != NULL)
-			head->next->type = DELIMITER;
-		(1) && (head->type = HEREDOC, *flag = 0);
-	}
-	else if (ft_strncmp(head->token, "|", ft_strlen(head->token)) == 0)
-		(1) && (head->type = PIPE, *flag = 0);
-	else
-		ft_check_rest_of_else(head, flag);
 }
