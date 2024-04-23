19f70b082a6e749146e8e0de0836f10e2b824aa4
Achraf Ibn Cheikh <aibn-che@e3r11p3.1337.ma>
2024-04-23 17:11:11 +0100

    this file purpose is to check filename, and render the correct error msg

---
+++ src/ft_check_file_name.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_file_name.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aibn-che <aibn-che@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 16:37:37 by aibn-che          #+#    #+#             */
+/*   Updated: 2024/04/23 17:05:59 by aibn-che         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	ft_add_back(t_file **lst, t_file *_new)
+{
+	t_file	*ptr;
+
+	if (lst == NULL)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = _new;
+		return ;
+	}
+	ptr = *lst;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	ptr->next = _new;
+}
+
+int	ft_contain_spaces(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i] && ft_strchr(" \t\n\v\f\r", str[i]))
+		i++;
+	if (str && !str[i])
+		return (1);
+	while (str && str[i] && !ft_strchr(" \t\n\v\f\r", str[i]))
+	{
+		i++;
+	}
+	while (str && str[i])
+	{
+		if (!ft_strchr(" \t\n\v\f\r", str[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	contain_quotes(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+/***
+ * Checking by file_name
+ * 
+ * 1 for ambiguous        (when the variable name not exist)    || var is null
+ * 2 for No such File     (when the variable name not exist And enclosed with double quotes) || var is null
+ * 0 regulare outfile
+*/
+int	ft_check_file_name(t_file *head)
+{
+	while (head)
+	{
+		if (contain_quotes(head->before))
+		{
+			if (head->after[0] == '\0')
+			{
+				printf("No such file or directory \n");
+				head->status = 2;
+				return (1);
+			}
+		}
+		else
+		{
+			if (head->after[0] == '\0' || ft_contain_spaces(head->after))
+			{
+				printf("ambiguous \n");
+				head->status = 1;
+				return (1);
+			}
+		}
+		head = head->next;
+	}
+	return (0);
+}
