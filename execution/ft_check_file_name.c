48768882fd35ba747a4e00f67571a0aa80dec5fe
Achraf Ibn Cheikh <aibn-che@e3r3p6.1337.ma>
2024-04-29 15:41:30 +0100

    fixing struct

--- execution/ft_check_file_name.c
+++ execution/ft_check_file_name.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check_file_name.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iassil <iassil@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aibn-che <aibn-che@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 16:37:37 by aibn-che          #+#    #+#             */
-/*   Updated: 2024/04/26 16:07:19 by iassil           ###   ########.fr       */
+/*   Updated: 2024/04/29 15:40:47 by aibn-che         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,15 +42,6 @@ int	ft_contain_quotes(char *str)
 	}
 	return (0);
 }
-
-typedef struct s_check_filename
-{
-	int		i;
-	int		j;
-	int		flag;
-	t_file	*head;
-	char	*tmp;
-}			t_check_filename;
 
 /*
  * Checking by file_name
